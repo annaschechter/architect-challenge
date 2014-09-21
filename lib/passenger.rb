ef9ab89dbc83ce6f56ca7039572f38d9baca6239
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-09-21 18:11:06 +0100

    Passengers can touch in and out

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -8,4 +8,12 @@ class Passenger
 		@account > 2.00
 	end
 
+	def touch_in(station)
+		station.let_in(self)
+	end
+
+	def touch_out(station)
+		station.let_out(self)
+	end
+
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,4 +1,5 @@
 require 'passenger'
+require 'station'
 
 describe Passenger do 
 
@@ -11,5 +12,22 @@ describe Passenger do
     	passenger = Passenger.new(:account => 1)
     	expect(passenger).to_not be_travelling
     end
-	
+
+    it "should be able to enter a station with account ballance over £2" do
+    	passenger = Passenger.new(:account => 2.50)
+    	station = Station.new
+    	expect(station.passenger_count).to eq(0)
+    	passenger.touch_in(station)
+        expect(station.passenger_count).to eq(1)
+    end
+
+    it "should be able to leave a station" do
+    	passenger = Passenger.new(:account => 2.50)
+    	station = Station.new
+    	expect(station.passenger_count).to eq(0)
+    	passenger.touch_in(station)
+    	passenger.touch_out(station)
+    	expect(station.passenger_count).to eq(0)
+    end
+
 end
